1f60459104eeebad286a8a4fecebba8655c48e60
Enno Lerebulan <lerebulanenno@gmail.com>
2025-09-22 16:21:46 +0800

    tambahkan backend, model pasien, dan rute API

---
+++ rekam-medis-api/models/patients.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const visitSchema = new Schema({
+    date: { type: Date, default: Date.now },
+    diagnosis: { type: String, required: true },
+    prescription: { type: String, required: true }
+});
+
+const patientSchema = new Schema({
+    name: { type: String, required: true },
+    dob: { type: Date, required: true },
+    gender: { type: String, required: true },
+    visits: [visitSchema]
+}, {
+    timestamps: true,
+});
+
+const Patient = mongoose.model('Patient', patientSchema);
+module.exports = Patient;
---
+++ rekam-medis-api/routes/patients.js
@@ -0,0 +1,40 @@
+const router = require('express').Router();
+let Patient = require('../models/Patient');
+
+router.route('/').get((req, res) => {
+    Patient.find()
+        .then(patients => res.json(patients))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/add').post((req, res) => {
+    const newPatient = new Patient(req.body);
+    newPatient.save()
+        .then(() => res.json('Pasien berhasil ditambahkan!'))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').get((req, res) => {
+    Patient.findById(req.params.id)
+        .then(patient => res.json(patient))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/visit/:id').post((req, res) => {
+    Patient.findById(req.params.id)
+        .then(patient => {
+            patient.visits.push(req.body);
+            patient.save()
+                .then(() => res.json('Kunjungan berhasil ditambahkan!'))
+                .catch(err => res.status(400).json('Error: ' + err));
+        })
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').delete((req, res) => {
+    Patient.findByIdAndDelete(req.params.id)
+        .then(() => res.json('Pasien berhasil dihapus.'))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+module.exports = router;
